71119acd8af3401c1f1fad376ec9f9348219723a
Edward <gillman.edward@gmail.com>
2022-09-28 12:04:33 +0100

    Correct version of ASEP Policy added. Demo updated.

--- ACTeN/src/approximations/policy_mps_asep.py
+++ ACTeN/src/approximations/policy_mps_asep.py
@@ -1,7 +1,6 @@
 import jax.numpy as jnp
-from jax import grad, jit, vmap, value_and_grad
 from jax import random
-from jax.lax import cond, fori_loop, scan
+from jax.lax import cond, scan
 from jax.config import config
 config.update("jax_enable_x64", True)
 
@@ -39,11 +38,9 @@ def _step_to_right(carry, input):
 
     return (weights, mat, environments, state), output
 
-def _step_to_right_boundary(carry, input):
-    """performs a step to the right for the asep. If the current state and the next one are in 01 or 10, can flip both.
-    If the local constraint condition is true, then the left-environment for the site is created and stored.
-    Until reach a site where the constraint is satisfied, the environment just consists of no-flips.
-
+def _add_zero_mat_step_to_right(carry, input):
+    """multiples on the zero-mat, A0, for the current site_idx (the input).
+        Used in a scan the final output mat will be a product of all zeros for the chosen inputs.
     Args:
         carry (_type_): _description_
         input (_type_): _description_
@@ -55,17 +52,7 @@ def _step_to_right_boundary(carry, input):
     weights, mat, environments, state = carry
     site_idx = input
 
-    cond_first_site = (site_idx == 0)
-
-    def true_func(mat, weights, environments):
-        mat = jnp.matmul(mat, weights[1,state[site_idx],:,:]) #Attach the take-flip matrix
-        return mat
-
-    def false_func(mat, weights, environments):
-        mat = jnp.matmul(mat, weights[0,state[site_idx],:,:]) #Attach the no-flip
-        return mat
-
-    mat = cond(cond_first_site, true_func, false_func, mat, weights, environments) #update the left-environments
+    mat = jnp.matmul(mat, weights[0,state[site_idx],:,:]) #Attach the no-flip matrix to continue
 
     output = None
 
@@ -99,68 +86,66 @@ def _get_environments_asep(state, weights):
         _type_: _description_
     """
 
+    def cond_no_flip_possible(state):
+        """The condition for the no-flip action to be possible.
+        Here, no flip can occur unless no particle has a neighbour"""
+        return 1. - jnp.allclose(jnp.sum(state*jnp.roll(state, 1)), 0)
+
+    ## Sweep to right
     environments = jnp.zeros((len(state)+1,weights.shape[2],weights.shape[3]))
 
     mat = jnp.identity(weights.shape[2])
 
-
     carry = (weights, mat, environments, state)
 
-    inputs = jnp.array(range(len(state)))
+    inputs = jnp.array(range(1,len(state))) #skip the first site at the action-matrix here depends on the final site
     carry, outputs = scan(_step_to_right, carry, inputs) #construct the left-environments via a rightward sweep
 
-    weights, mat, environments, state = carry
+    weights, mat, environments, state = carry ## mat from the rightward sweep is the no-flip factor but missing the first (site=0) matrix
 
-    #Add condition for no flip to be impossibe
+    ## If No Flip is possible, set environment != 0
 
-    # cond_all_up = jnp.prod(state) #used to exclude the no-flip action case if true
-    # no_flip_is_possible = cond_all_up != 1
-    # no_flip_is_possible = True
+    def no_flip_possible_true_func(environments):
+        environments = environments.at[-1,:,:].set(jnp.matmul(weights[0,state[0],:,:],mat)) #add the first no-flip matrix to complete env.
+        return environments
 
-    # def true_func(environments): #no flip is possible
-    #     environments = environments.at[-1,:,:].set(jnp.identity(weights.shape[2]))
-    #     return environments
+    def no_flip_possible_false_func(environments):
+        environments = environments.at[-1,:,:].set(jnp.zeros(environments.shape[1::])) #add the first no-flip matrix to complete env.
+        return environments
 
-    # def false_func(environments): #no flip is impossible. keep environment as zero.
-    #     return environments
+    environments = cond(cond_no_flip_possible(state), no_flip_possible_true_func, no_flip_possible_false_func, environments)
 
-    # environments = cond(no_flip_is_possible, true_func, false_func, environments)
-
-    environments = environments.at[-1,:,:].set(jnp.identity(weights.shape[2])) #no flip always possible for bond asep
-
+    ## Sweep to left
     mat = jnp.identity(weights.shape[2])
 
     carry = (weights, mat, environments, state)
 
-    inputs = jnp.array(range(len(state)-2, -1, -1)) #resolve special case for site=len(state)-1 last
+    inputs = jnp.array(range(len(state)-1, 0, -1)) #Exclude site=0 as need explicit construction (was skipped in right-sweep)
     carry, outputs = scan(_step_to_left, carry, inputs)
 
     weights, mat, environments, state = carry
 
-    #Finish no-flip
+    ## Site-0 prob still needs computing: perform manually
 
-    environment_completed_no_flip =  jnp.matmul(weights[0,state[0],:,:], mat) #Attach the no-flip matrix to complete the no-flip environment
-    environments = environments.at[-1,:,:].set(environment_completed_no_flip)
-
-    #Simply calculate special case manually
-
-    cond_last_site_can_flip = (state[-1] != state[0]) #the local constraint to enable flip of last state and first in PBC, requires special handling
+    cond_zero_site_can_flip = (state[0] != state[1]) #the local constraint to enable flip of last state and first in PBC, requires special handling
 
     def true_func(environments): #flip on final site and 0 is possible
-        environments = environments.at[-2,:,:].set(jnp.identity(weights.shape[2])) #-1 is no flip, so -2 is the final flip. Eg. for L = 4; a = (1,0,0,1)
+        environments = environments.at[0,:,:].set(jnp.identity(weights.shape[2]))
         return environments
 
-    def false_func(environments): #cannot flip final site and 0
+    def false_func(environments): #cannot flip 0 and 1
         return environments
 
-    environments = cond(cond_last_site_can_flip, true_func, false_func, environments) # reset environment for -2 as rightward sweep was incorrect in this case
+    environments = cond(cond_zero_site_can_flip, true_func, false_func, environments) #
 
     mat = jnp.identity(weights.shape[2])
-    inputs = jnp.array(range(len(state)-1))
-    carry, outputs = scan(_step_to_right_boundary, carry, inputs)
-    weights, mat, _, state = carry
-    environment_completed_boundary_flip =  jnp.matmul(mat, weights[1,state[-1],:,:]) #Attach the no-flip matrix to complete the no-flip environment
-    environments = environments.at[-2,:,:].set(environment_completed_boundary_flip)
+    inputs = jnp.array(range(2, len(state))) #for these inputs the final mat with have zeros for all sites except the left and right boundaries
+    carry, outputs = scan(_add_zero_mat_step_to_right, carry, inputs)
+    weights, mat, _, state = carry #mat is the product of 0s in the bulk
+    flip_mat_01 = jnp.matmul(weights[1,state[0],:,:], weights[1,state[1],:,:])
+    completed_01_flip_environment = jnp.matmul(flip_mat_01,mat)
+    completed_01_environment = jnp.matmul(environments[0,:,:], completed_01_flip_environment)
+    environments = environments.at[0,:,:].set(completed_01_environment)
 
     return environments
 
@@ -178,22 +163,7 @@ def policy_sample(state, key, weights):
     probability = probs[action]
     return action, key, probability
 
-# def policy(state, key, weights):
-#     chi_policy = weights.shape[-1]
-#     tensor = (jnp.array([[jnp.eye(chi_policy), jnp.eye(chi_policy)],
-# 							 [jnp.eye(chi_policy), jnp.eye(chi_policy)]]) + weights)
-#     environments = _get_environments_east_model(state, tensor)
-#     probs, _ = _get_probs(environments)
-#     key, subkey = random.split(key)
-#     action = random.choice(subkey, jnp.array(list(range(len(state)+1))), p = probs) #Use shape?
-#     probability = probs[action]
-#     log_prob = jnp.log(probability)
-#     return log_prob, (action, key)
-
 def policy(state, key, weights):
-    # chi_policy = weights.shape[-1]
-    # tensor = (jnp.array([[jnp.eye(chi_policy), jnp.eye(chi_policy)],
-	# 						 [jnp.eye(chi_policy), jnp.eye(chi_policy)]]) + weights)
     tensor = weights
     environments = _get_environments_asep(state, tensor)
     probs, _ = _get_probs(environments)
